120fd4cff2aa803208645c7e8647dd4ec1f19edb
Jonathan Ma <majo@oregonstate.edu>
2021-05-15 13:37:44 -0700

    In palindrome.py, added code that removed spaces from the string

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,13 @@ def main():
     # Get user input and convert it to lower case.
     userInput = input("Enter your word to test for palindrome: ").lower()
 
+    cleanedString = ''
 
+    for char in userInput:
+        if char != ' ':
+            cleanedString += char
+
+    print(cleanedString)
 
 if __name__ == "__main__":
     main()
