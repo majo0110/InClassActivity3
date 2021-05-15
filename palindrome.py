e672752a78be3859c91fde208d178b3ec4dfd006
Jonathan Ma <majo@oregonstate.edu>
2021-05-15 13:29:07 -0700

    added user input to palindrome.py and convert it to lower case

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+def main():
+    # Get user input and convert it to lower case.
+    userInput = input("Enter your word to test for palindrome: ").lower()
+
+
+
+if __name__ == "__main__":
+    main()
