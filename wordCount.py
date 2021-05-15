462bf45bdc6bebd2e27ccf5ab5330bf4dd71bf43
Jonathan Ma <majo@oregonstate.edu>
2021-05-15 14:19:37 -0700

    Changed output of wordCount.py to just the len of sentence.

--- wordCount.py
+++ wordCount.py
@@ -2,7 +2,7 @@ def main():
     userInput = input("Enter your word to sentence to see word count: ")
     splitInput = userInput.split()
 
-    print("Your word count is:",len(splitInput))
+    print(len(splitInput))
 
 if __name__ == "__main__":
     main()
