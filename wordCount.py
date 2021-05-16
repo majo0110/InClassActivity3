5afad616114581211ae4e8247904d0170e097523
Jonathan Ma <majo@oregonstate.edu>
2021-05-15 20:44:16 -0700

    Changed wordCount.py from main to wordCounter()

--- wordCount.py
+++ wordCount.py
@@ -1,8 +1,13 @@
 def main():
+    print(wordCounter())
+
+def wordCounter():
+    # Get user input and split it into a list
     userInput = input("Enter your word to sentence to see word count: ")
     splitInput = userInput.split()
 
-    print(len(splitInput))
+    # Return the length of list
+    return(len(splitInput))
 
 if __name__ == "__main__":
     main()
